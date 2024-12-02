7a1e81a44c6aec04f75f9128046572c379ae1a87
Nandiniatri <atrinandini8@gmail.com>
2024-12-02 16:57:18 +0530

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,34 @@
+// rollup.config.js
 import babel from 'rollup-plugin-babel';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.js',  // Entry point
   output: [
     {
-      file: 'dist/react-package.cjs.js',
+      file: 'dist/react-package.cjs.js',  // CommonJS build
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: 'dist/react-package.esm.js',
+      file: 'dist/react-package.esm.js',  // ES Module build
       format: 'esm',
       sourcemap: true,
     },
     {
-      file: 'dist/react-package.umd.js',
+      file: 'dist/react-package.umd.js',  // UMD build for browsers
       format: 'umd',
       name: 'ReactPackage',
-      globals: { react: 'React' }, // Fix यही है
       sourcemap: true,
     },
   ],
   plugins: [
-    peerDepsExternal(),
+    peerDepsExternal(),  // Exclude React, ReactDOM from the bundle
     babel({
       exclude: 'node_modules/**',
       presets: ['@babel/preset-env', '@babel/preset-react'],
     }),
-    terser(),
+    terser(),  // Minify UMD build
   ],
-  external: ['react'], // React को external रखो
 };
